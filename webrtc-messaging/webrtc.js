805f3098b9df4ef4a125d835c22efa88358dd4af
Ilia Ovsiannikov <ilia@oomwoo.com>
2018-12-08 02:07:41 -0800

    WebRTC Messaging to kaia-utils@0.1.0.js

--- webrtc-messaging/webrtc.js
+++ webrtc-messaging/webrtc.js
@@ -1,20 +1,19 @@
 import { createMessaging } from 'https://cdn.jsdelivr.net/npm/kaia-services.js@0.0.3/dist/kaia-services.mjs';
-let peerConnection, uuid, messaging, name, dataChannel;
+import { createWebRTCHelper } from 'https://cdn.jsdelivr.net/npm/kaia-utils.js@0.1.0/dist/kaia-utils.mjs';
+let messaging, userName, webrtc;
 const roomName = 'webrtc-messaging';
-let webRtcStarted = false;
-let isCaller = false;
-const peerConnectionConfig = {
-  'iceServers': [
-    {'urls': 'stun:stun.stunprotocol.org:3478'},
-    {'urls': 'stun:stun.l.google.com:19302'},
-  ]
-};
-const dataChannelOptions = {
-  ordered: false, // do not guarantee order
-  maxPacketLifeTime: 3000, // in milliseconds
-};
 
 pageReady();
+
+async function pageReady() {
+  userName = prompt("What's your name?");
+
+  setupGui();  
+  messaging = await createMessaging({ io: io(), rooms: roomName });
+  insertMessageToDOM({content: 'Waiting for peer to join'});
+  webrtc = await createWebRTCHelper({ messaging: messaging, eventListener: onWebRTCEvent, room: roomName });
+  insertMessageToDOM({content: 'Peer has joined'});
+}
 
 function setupGui() {
   const form = document.querySelector('form');
@@ -24,148 +23,36 @@ function setupGui() {
     input.value = '';
 
     const data = {
-      name,
-      content: value
+      userName,
+      content: value,
     };
 
-    const json = JSON.stringify(data);
-    dataChannel.send(json);
-
-    insertMessageToDOM(data, true);
+    if (webrtc && webrtc.dataChannel()) {
+      webrtc.send(JSON.stringify(data));
+      insertMessageToDOM(data, true);
+    }
   });
 }
 
-function pageReady() {
-  name = prompt("What's your name?");
-
-  setupGui();  
-  connectMessaging();
-  uuid = createUUID();
-  insertMessageToDOM({content: 'Waiting for peer to join'});
-}
-
-function start(isCaller) {
-  peerConnection = new RTCPeerConnection(peerConnectionConfig);
-  peerConnection.onicecandidate = gotIceCandidate;
-
-  if (isCaller) {    
-    dataChannel = peerConnection.createDataChannel('chat');
-    setupDataChannel();    
-    peerConnection.createOffer().then(createdDescription).catch(errorHandler);
-  } else {
-    // If user is not the offerer let's wait for a data channel
-    peerConnection.ondatachannel = event => {
-      dataChannel = event.channel;
-      setupDataChannel();
-    };
-  }
-}
-
-function checkDataChannelState() {
-  const s = 'Data channel is ' + dataChannel.readyState;
-  insertMessageToDOM({content: s});
-}
-
-function setupDataChannel() {
-  dataChannel.onopen = checkDataChannelState;
-  dataChannel.onclose = checkDataChannelState;
-  dataChannel.onmessage = event =>
-    insertMessageToDOM(JSON.parse(event.data), false);
-  dataChannel.onerror = (error) =>
-    insertMessageToDOM({content: 'Data channel error: ' + error}); 
-}
-
-
-function gotMessageFromServer(message) {
-  if(!peerConnection) start(false);
-
-  var signal = message;
-
-  // Ignore messages from ourself
-  if(signal.uuid == uuid)
-    return;
-
-  if (signal.sdp) {
-    peerConnection.setRemoteDescription(new RTCSessionDescription(signal.sdp)).then(function() {
-      // Only create answers in response to offers
-      if(signal.sdp.type == 'offer') {
-        peerConnection.createAnswer().then(createdDescription).catch(errorHandler);
-      }
-    }).catch(errorHandler);
-  } else if(signal.ice) {
-    peerConnection.addIceCandidate(new RTCIceCandidate(signal.ice)).catch(errorHandler);
-  }
-}
-
-function gotIceCandidate(event) {
-  if (event.candidate != null)
-    messaging.send({'ice': event.candidate, 'uuid': uuid});
-}
-
-function createdDescription(description) {
-  console.log('got description');
-
-  peerConnection.setLocalDescription(description).then(function() {
-    messaging.send({'sdp': peerConnection.localDescription, 'uuid': uuid});
-  }).catch(errorHandler);
-}
-
-function errorHandler(error) {
-  console.log(error);
-  insertMessageToDOM({content: 'Error: ' + error}); 
-}
-
-function createUUID() {
-  function s4() {
-    return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
-  }
-
-  return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
-}
-
-async function connectMessaging() {
-  try {
-    messaging = await createMessaging({ io: io(), eventListener: onMessageEvent, rooms: roomName });
-  } catch (e) {
-    console.log('Exception ' + JSON.stringify(e));
-  } finally {
-    //console.log('connectMessaging() finished');
-  }
-}
-
-function onMessageEvent(err, msg) {
-  if (err)
-    return;
+function onWebRTCEvent(err, msg) {
   switch (msg.event) {
-    case 'message':
-      gotMessageFromServer(msg.message);
+    case 'dataChannelMessage':
+      insertMessageToDOM(JSON.parse(event.data), false);
       break;
-    case 'joined':
-      if (webRtcStarted)
-        return;
-      const n = msg.clients.length;
-      if (n >= 3)
-        return alert('The room is full');
-      
-      // First to enter the room is caller
-      if (n == 1)
-        isCaller = true;
-      
-      // Wait both peers to join
-      if (n !== 2)
-        return;
-
-      webRtcStarted = true;
-      start(isCaller);
+    case 'dataChannelOpen':
+    case 'dataChannelClose':
+      insertMessageToDOM({content: ('Data channel is ' + msg.dataChannel.readyState)});
       break;
-    }
+    case 'dataChannelError':
+      insertMessageToDOM({content: 'Data channel error: ' + msg.err}); 
+  }
 }
 
 function insertMessageToDOM(options, isFromMe) {
   const template = document.querySelector('template[data-template="message"]');
   const nameEl = template.content.querySelector('.message__name');
-  if (options.name)
-    nameEl.innerText = options.name;
+  if (options.userName)
+    nameEl.innerText = options.userName;
   template.content.querySelector('.message__bubble').innerText = options.content;
   const clone = document.importNode(template.content, true);
   const messageEl = clone.querySelector('.message');
